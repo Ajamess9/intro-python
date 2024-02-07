f9207e852ce4a1720ab8f552583f5dc1ede11041
Ajamess9 <158079580+Ajamess9@users.noreply.github.com>
2024-02-07 12:06:58 -0500

    activityFeb7.py

---
+++ activityFeb7.py
@@ -0,0 +1,36 @@
+# 1. In your own words, what is a computer program? I think computer programs is when the computers take your idea and put them inside the system.
+
+# 2. Name three (3) data types 1. integers. 2 strings. float
+
+# 3. Name three (3) Python operators 1. identity operators 2. logical operators 3. membership operators
+
+# 4. In your own words, what is a variable? a variable is something you you're to store storage for your computer  
+
+# READING PROMPTS
+# Carefully read through each scenario and determine which Python Operator you would use to build the
+# particular program. 
+
+# Read the question carefully
+# Find keywords that you recognized to deduce what it could be
+# use context clues provided in the prompt to deduce what it could be.
+
+# 5. You have been hired as a software engineer to develop a gym membership system.
+# Your program needs to verify two (2) things; if the user is a member of the gym, AND
+# if the user has the gold membership tier, which allows the user to have access to the swimming pool.
+# What your client would like to happen is that when a user is a member AND 
+# and also has the gold membership they should be able to use the pool.
+# Which Python operator would you use in this situation? Explain why I think the Python operator I would use the membership operator to test if it's a specific sequence. 
+
+# 6. You have been hired as a software engineer to develop a simple payment system for a company 
+# webstore. Your assignment is to create a cart system where the prices of all 
+# the items are added together and then deducted a 20% discount at checkout. 
+# Which Python operator would you use in this situation? Explain why. identity operator because I would identify their membership for them to get in and go but if they are not in the system then they would not be able to get in there.
+
+# 7. You have been hired as an engineer to develop a student badge system. Your client would like to 
+# have a system where when a student swipes their badge, the ID number on the badge is compared to the ID
+# number in the database.If the ID numbers match and are the same the student can come in, if it does not
+# match, they cannot enter.
+# Which Python operator would you use in this situation? Explain why I would use Python arithmetic operators to do this because they seem to do the most and have more power to control every thing
+ 
+
+ 
